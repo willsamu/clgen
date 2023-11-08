ad05274a2e1fbaa5b0940c65ef47658fc678843c
Samuel Will <will.samuel@protonmail.com>
2023-11-08 14:36:22 +0200

    feat: Add CV management commands

--- clgen.py
+++ clgen.py
@@ -1,6 +1,6 @@
 # clgen.py
 import click
-from config import configure_profile
+from config import configure_profile, add_cv, delete_cv, display_cv
 
 
 @click.group()
@@ -13,6 +13,24 @@ def clgen():
 def configure():
     """Configure user profile."""
     configure_profile()
+
+
+@clgen.command()
+def cv_add():
+    """Add a new CV."""
+    add_cv()
+
+
+@clgen.command()
+def cv_delete():
+    """Delete an existing CV."""
+    delete_cv()
+
+
+@clgen.command()
+def cv_display():
+    """Display an existing CV."""
+    display_cv()
 
 
 if __name__ == "__main__":
--- config.py
+++ config.py
@@ -36,3 +36,52 @@ def _ask_and_set_config():
     set_key(".env", "COUNTRY", country)
 
     click.echo("Configuration saved.")
+
+
+def add_cv():
+    cv_name = click.prompt("Enter the name for the CV")
+    cv_content = click.edit("Enter the CV content (multiline supported):")
+    if cv_content:
+        cv_path = f"cvs/{cv_name}.txt"
+        with open(cv_path, "w") as cv_file:
+            cv_file.write(cv_content)
+        click.echo(f'CV "{cv_name}" saved.')
+
+
+def delete_cv():
+    cvs = _get_cv_list()
+    if cvs:
+        for idx, cv in enumerate(cvs, start=1):
+            click.echo(f"{idx}. {cv}")
+        cv_index = click.prompt("Enter the number of the CV to delete", type=int)
+        if 0 < cv_index <= len(cvs):
+            os.remove(f"cvs/{cvs[cv_index - 1]}")
+            click.echo(f'CV "{cvs[cv_index - 1]}" deleted.')
+        else:
+            click.echo("Invalid CV number.")
+    else:
+        click.echo("No CVs found.")
+
+
+def display_cv():
+    cvs = _get_cv_list()
+    if cvs:
+        for idx, cv in enumerate(cvs, start=1):
+            click.echo(f"{idx}. {cv}")
+        cv_index = click.prompt("Enter the number of the CV to display", type=int)
+        if 0 < cv_index <= len(cvs):
+            cv_path = f"cvs/{cvs[cv_index - 1]}"
+            with open(cv_path, "r") as cv_file:
+                click.echo(cv_file.read())
+        else:
+            click.echo("Invalid CV number.")
+    else:
+        click.echo("No CVs found.")
+
+
+def _get_cv_list():
+    return [cv for cv in os.listdir("cvs") if cv.endswith(".txt")]
+
+
+# Make sure the cvs directory exists
+os.makedirs("cvs", exist_ok=True)
