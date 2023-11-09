8e5108e3d24f478cbb5014b463f245f015976a8a
Samuel Will <will.samuel@protonmail.com>
2023-11-09 12:35:35 +0200

    feat: prepare for pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 # setup.py
+import os
+import codecs
 from setuptools import setup, find_packages
 
-packages = find_packages()
-print(packages)
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\\n" + fh.read()
 
 setup(
     name="clgen",
-    version="0.1.0",
+    version="{{VERSION_PLACEHOLDER}}",
+    author="Samuel Will",
+    author_email="will.samuel@protonmail.com",
+    description="OpenAI GPT based cover letter generator.",
+    url="https://github.com/willsamu/clgen",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -21,4 +31,12 @@ setup(
         [console_scripts]
         clgen=clgen.clgen:clgen
     """,
+    keywords=["OpenAI", "CV", "cover letter", "ai generator"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+    ],
 )
